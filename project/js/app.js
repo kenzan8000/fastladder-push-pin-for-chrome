a75e0f5160107943984a81c1052e5889f56d3f0f
kenzan8000 <kenzan8000@gmail.com>
2017-12-16 23:41:45 -0800

    add storage permission and fastladder url local storage

--- project/js/app.js
+++ project/js/app.js
@@ -1,4 +1,21 @@
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+
+        //var link = tabs[0].url;
+        //var title = tabs[0].title;
+
+        var defaults = { fastladder: { url: '' } };
+        chrome.storage.local.get(defaults, function(items) {
+            // check if fastladder url is valid
+            var fastladderUrl  = document.createElement('a');
+            fastladderUrl.href = items.fastladder.url;
+            if (fastladderUrl.protocol !== 'https:' && fastladderUrl.protocol !== 'http:') {
+            }
+        });
+
+        //var entity = { fastladder: { url: '' } };
+        //chrome.storage.local.set(entity, function() {
+        //});
+
     });
 });
